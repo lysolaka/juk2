42ad830b06c0a699a94468206563ad2fa5e0c84f
lysolaka <lysolaka@gmail.com>
2026-02-09 12:45:02 +0100

    feat: implement UART echo

--- juk-firmware/src/main.rs
+++ juk-firmware/src/main.rs
@@ -9,9 +9,11 @@
 extern crate alloc;
 
 use embassy_executor::Spawner;
-use embassy_time::Timer;
 use esp_backtrace as _;
-use esp_hal::timer::timg::TimerGroup;
+use esp_hal::{
+    timer::timg::TimerGroup,
+    uart::{Config, DataBits, Parity, StopBits, Uart},
+};
 use esp_println as _;
 
 esp_bootloader_esp_idf::esp_app_desc!();
@@ -29,8 +31,39 @@ async fn main(spawner: Spawner) -> ! {
     // TODO: Spawn some tasks
     let _ = spawner;
 
+    let uart_config = Config::default()
+        .with_baudrate(115200)
+        .with_data_bits(DataBits::_8)
+        .with_stop_bits(StopBits::_1)
+        .with_parity(Parity::None);
+
+    let mut uart = defmt::expect!(
+        Uart::new(peripherals.UART0, uart_config),
+        "Failed to initialize the UART interface"
+    )
+    .into_async();
+    let mut buf = [0; 128];
+
     loop {
-        defmt::info!("Hello world!");
-        Timer::after_secs(10).await;
+        match uart.read_async(&mut buf).await {
+            Ok(0) => defmt::panic!("UART read returned 0"),
+            Ok(read) => {
+                defmt::info!("UART in: {=[u8]:02x}", &buf[..read]);
+                let mut count = 0;
+                while count != read {
+                    match uart.write_async(&buf[count..read]).await {
+                        Ok(written) => count += written,
+                        Err(e) => {
+                            defmt::error!("UART error: {}", e);
+                            defmt::panic!("Bye, bye!");
+                        }
+                    }
+                }
+            }
+            Err(e) => {
+                defmt::error!("UART error: {}", e);
+                defmt::panic!("Bye, bye!");
+            }
+        }
     }
 }

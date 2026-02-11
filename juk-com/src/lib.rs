fa5838db0b87e7474624130b7eb56a05a7f481be
lysolaka <lysolaka@gmail.com>
2026-02-11 17:37:21 +0100

    docs(juk-com): document the crate root

--- juk-com/src/lib.rs
+++ juk-com/src/lib.rs
@@ -1,3 +1,8 @@
+//! Communication interface for use with the `juk-firmware`.
+//!
+//! Implements the [`Interface`] struct meant to handle control input and the [`Terminal`] trait
+//! used to perform actions required by [`Interface`].
+
 #![no_std]
 
 extern crate alloc;
@@ -8,12 +13,23 @@ mod terminal;
 
 use alloc::{string::String, vec::Vec};
 
+/// An enum representing input events fired by [`Interface`].
 pub enum Input {
+    /// Binary data was recieved.
+    ///
+    /// The sentinel NUL byte is also included in the payload.
     Binary(Vec<u8>),
+    /// Text data was recieved.
+    ///
+    /// The payload is a stripped string.
     Text(String),
+    /// CTRL + G was pressed.
     Bell,
+    /// CTRL + X was pressed.
     Cancel,
+    /// CTRL + C was pressed, the input buffer was cleared, redraw the prompt.
     EndOfText,
+    /// CTRL + D was pressed.
     EndOfTransmission,
 }
 

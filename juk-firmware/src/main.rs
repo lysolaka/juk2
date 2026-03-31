5a134794a42aff2324956f6268f55269f63f9177
lysolaka <lysolaka@gmail.com>
2026-03-31 10:25:12 +0200

    feat(juk-firmware): start both cores and run async executors

--- juk-firmware/src/main.rs
+++ juk-firmware/src/main.rs
@@ -8,75 +8,100 @@
 
 extern crate alloc;
 
-use embassy_executor::Spawner;
-use esp_backtrace as _;
 use esp_hal::{
-    timer::timg::TimerGroup,
-    uart::{Config, DataBits, Parity, StopBits, Uart},
+    interrupt::software::SoftwareInterruptControl,
+    system::Stack,
+    timer::systimer::SystemTimer,
 };
+
+use esp_rtos::embassy::Executor;
+
+use esp_backtrace as _;
 use esp_println as _;
-use juk_com::{Input, Interface, Terminal};
+
+use static_cell::{ConstStaticCell, StaticCell};
+
+/// Main core executor
+static MAIN_EXECUTOR: StaticCell<Executor> = StaticCell::new();
+/// Application core stack (128K)
+static APP_STACK: ConstStaticCell<Stack<131072>> = ConstStaticCell::new(Stack::new());
+/// Application core executor
+static APP_EXECUTOR: StaticCell<Executor> = StaticCell::new();
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
-#[esp_rtos::main]
-async fn main(spawner: Spawner) -> ! {
+// esp_hal::assign_resources! {
+//     Resources<'d> {
+//         led: LedResources<'d> {
+//             r: GPIO17,
+//             g: GPIO18,
+//             b: GPIO8,
+//             ctrl: LEDC,
+//         },
+//         com: ComResources<'d> {
+//             uart: UART0,
+//         },
+//         motor: MotorResources<'d> {
+//             en_drv: GPIO45,
+//             en_led: GPIO21,
+//             x_step: GPIO10,
+//             x_dir: GPIO9,
+//             y_step: GPIO12,
+//             y_dir: GPIO11,
+//             z_step: GPIO13,
+//             z_dir: GPIO14,
+//         },
+//         limits: LimitsResources<'d> {
+//             x_p: GPIO4,
+//             x_m: GPIO5,
+//             y_p: GPIO6,
+//             y_m: GPIO7,
+//             z_p: GPIO15,
+//             z_m: GPIO16,
+//         },
+//     }
+// }
+
+#[esp_hal::main]
+fn main() -> ! {
+    // we configure the clocks and etc. here to avoid doing it later
+    // the default config is the best config
     let config = esp_hal::Config::default();
     let peripherals = esp_hal::init(config);
 
+    // see `lib.rs` for why this is such a pain
+    // let resources = split_resources!(peripherals);
+
+    // turn on the heap allocator
     esp_alloc::psram_allocator!(peripherals.PSRAM, esp_hal::psram);
 
-    let timg0 = TimerGroup::new(peripherals.TIMG0);
-    esp_rtos::start(timg0.timer0);
+    // start the RTOS timer
+    let SystemTimer { alarm0, .. } = SystemTimer::new(peripherals.SYSTIMER);
+    esp_rtos::start(alarm0);
 
-    // TODO: Spawn some tasks
-    let _ = spawner;
+    // start the application core (still don't know why we need software interrupts for this)
+    let SoftwareInterruptControl {
+        software_interrupt0,
+        software_interrupt1,
+        ..
+    } = SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+    esp_rtos::start_second_core(
+        peripherals.CPU_CTRL,
+        software_interrupt0,
+        software_interrupt1,
+        APP_STACK.take(),
+        || {
+            let app_executor = APP_EXECUTOR.init(Executor::new());
+            app_executor.run(|_| {
+                // spawn the main task for the application core, which gets the spawner and can control its thread
+                // s.must_spawn(main1(s));
+            })
+        },
+    );
 
-    let uart_config = Config::default()
-        .with_baudrate(115200)
-        .with_data_bits(DataBits::_8)
-        .with_stop_bits(StopBits::_1)
-        .with_parity(Parity::None);
-
-    let mut uart = defmt::expect!(
-        Uart::new(peripherals.UART0, uart_config),
-        "Failed to initialize the UART interface"
-    )
-    .into_async();
-
-    let mut interface = Interface::new();
-
-    uwrite(&mut uart, "$ ").await;
-
-    loop {
-        match interface.get_input(&mut uart).await {
-            Ok(input) => match input {
-                Input::Binary(items) => defmt::info!("Binary input: {=[u8]}", &items[..]),
-                Input::Text(text) => {
-                    defmt::info!("Text input: {}", text.as_str());
-                    uwrite(&mut uart, "$ ").await;
-                }
-                Input::EndOfTransmission => {
-                    defmt::info!("CTRL + D: resetting...");
-                    esp_hal::system::software_reset();
-                }
-                _ => {
-                    uwrite(&mut uart, "$ ").await;
-                    defmt::expect!(interface.redraw_line(&mut uart).await, "UART write failed");
-                }
-            },
-            Err(e) => {
-                defmt::error!("UART Error: {}", e);
-                defmt::panic!();
-            }
-        }
-    }
-}
-
-/// Quick wrapper for UART writes using the [`Terminal`] trait.
-///
-/// NOTE: for testing purposes only.
-#[inline]
-async fn uwrite<T: Terminal>(term: &mut T, text: &str) {
-    defmt::expect!(term.write(text.as_bytes()).await, "UART write failed");
+    let main_executor = MAIN_EXECUTOR.init(Executor::new());
+    main_executor.run(|_| {
+        // spawn the main task for the main thread
+        // s.must_spawn(main0(s));
+    })
 }

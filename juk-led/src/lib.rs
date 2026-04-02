553dbc2604634c5d3170ef755973420a7a1eff0e
lysolaka <lysolaka@gmail.com>
2026-04-02 12:30:02 +0200

    feat(juk-led): implement a new RGB LED adapter

--- juk-led/src/lib.rs
+++ juk-led/src/lib.rs
@@ -1,185 +1,115 @@
 //! A simple RGB LED controller, which allows for setting a color.
 //!
-//! The [`LEDAdapter`] assumes that the RMT peripheral has been configured to run at 80MHz.
+//! # Example
 //!
-//! # Usage
-//!
+//! ```no_run
+//! use esp_hal::{
+//!     ledc::{
+//!         LSGlobalClkSource,
+//!         Ledc,
+//!         LowSpeed,
+//!         timer,
+//!         timer::TimerIFace,
+//!     },
+//!     time::Rate,
+//! };
+//! 
+//! use juk_led::LEDAdapter;
+//! 
+//! // configure the LEDC peripheral
+//! let mut ledc = Ledc::new(peripheral.LEDC);
+//! ledc.set_global_slow_clock(LSGlobalClkSource::APBClk);
+//! 
+//! // configure the timer
+//! let mut tim0 = ledc.timer::<LowSpeed>(timer::Number::Timer0);
+//! tim0.configure(timer::config::Config {
+//!     duty: timer::config::Duty::Duty8Bit,
+//!     clock_source: timer::LSClockSource::APBClk,
+//!     frequency: Rate::from_khz(25),
+//! })?;
+//! 
+//! // construct the adapter
+//! let mut rgb_led = LEDAdapter::new(&ledc, &tim0, red_pin, green_pin, blue_pin)?;
+//! 
+//! // use it
+//! rgb_led.set_color(0xff, 0x80, 0x80);
 //! ```
-//! use esp_hal::{Config, rmt:Rmt, time::Rate};
-//! use juk_led::{LEDAdapter, RGB};
-//!
-//! let peripherals = esp_hal::init(Config::default()); // get your peripherals
-//! let rmt = Rmt::new(peripherals.RMT, Rate::from_mhz(80)).unwrap(); // configure RMT
-//!
-//! let mut led = LEDAdapter::new(rmt.channel0, peripherals.GPIO38); // construct the adapter
-//! led.set_color(&RGB::new(0xff, 0x00, 0xff)); // display your favourite color
-//! ```
-
 #![no_std]
 
 use esp_hal::{
-    Async,
-    Blocking,
-    DriverMode,
-    gpio::{Level, interconnect::PeripheralOutput},
-    rmt::{Channel, PulseCode, Tx, TxChannelConfig, TxChannelCreator},
+    gpio::{DriveMode, interconnect::PeripheralOutput},
+    ledc::{
+        Ledc,
+        LowSpeed,
+        channel,
+        channel::{Channel, ChannelHW, ChannelIFace},
+        timer::{Timer, TimerHW, TimerIFace},
+    },
+    time::Rate,
 };
 
-// bit timings from the WS2812B datasheet
-const T0H: u32 = 350;
-const T0L: u32 = 800;
-
-const T1H: u32 = 700;
-const T1L: u32 = 600;
-
-// bit pulse codes calculated for an 80MHz peripheral clock
-const PULSE_0: PulseCode = PulseCode::new(
-    Level::High,
-    ((T0H * 80) / 1000) as u16,
-    Level::Low,
-    ((T0L * 80) / 1000) as u16,
-);
-
-const PULSE_1: PulseCode = PulseCode::new(
-    Level::High,
-    ((T1H * 80) / 1000) as u16,
-    Level::Low,
-    ((T1L * 80) / 1000) as u16,
-);
-
-/// A dead simple RGB 8-bit color representation.
-#[derive(defmt::Format, Clone, Copy)]
-pub struct RGB {
-    pub r: u8,
-    pub g: u8,
-    pub b: u8,
+/// An RGB LED adapter. Designed to control a common cathode LED.
+pub struct LEDAdapter<'d> {
+    r: Channel<'d, LowSpeed>,
+    g: Channel<'d, LowSpeed>,
+    b: Channel<'d, LowSpeed>,
 }
 
-impl RGB {
-    /// Constructor for the [`RGB`] struct, if it makes your code look better.
-    pub const fn new(r: u8, g: u8, b: u8) -> Self {
-        RGB { r, g, b }
-    }
-
-    /// Convert the [`RGB`] color to the required [`PulseCode`] sequence. The sequence will be
-    /// saved to `pulses`.
-    ///
-    /// Note that the color format of the WS2812B LED is GRB.
-    fn to_pulses(&self, pulses: &mut [PulseCode; 25]) {
-        for pos in 0..8 {
-            match self.g & (1 << pos) {
-                0 => pulses[pos] = PULSE_0,
-                _ => pulses[pos] = PULSE_1,
-            }
-        }
-        for pos in 0..8 {
-            match self.r & (1 << pos) {
-                0 => pulses[8 + pos] = PULSE_0,
-                _ => pulses[8 + pos] = PULSE_1,
-            }
-        }
-        for pos in 0..8 {
-            match self.b & (1 << pos) {
-                0 => pulses[16 + pos] = PULSE_0,
-                _ => pulses[16 + pos] = PULSE_1,
-            }
-        }
-    }
-}
-
-/// A WS2812B RGB LED driver.
-///
-/// This driver can work in synchronous and asyncronous modes depending on which driver mode the
-/// RMT peripheral was set up with.
-///
-/// Since this is an LED driver and not something critical all errors are handled for by
-/// emiting a warning message.
-pub struct LEDAdapter<'ch, Dm>
-where
-    Dm: DriverMode,
-{
-    channel: Option<Channel<'ch, Dm, Tx>>,
-    buffer: [PulseCode; 25],
-}
-
-impl<'ch, Dm> LEDAdapter<'ch, Dm>
-where
-    Dm: DriverMode,
-{
-    /// Returns the transmit channel configuration to be applied for the driver's RMT channel.
-    fn channel_config() -> TxChannelConfig {
-        TxChannelConfig::default()
-            .with_clk_divider(1)
-            .with_idle_output(true)
-            .with_idle_output_level(Level::Low)
-            .with_carrier_modulation(false)
-    }
-
-    /// Construct a new [`LEDAdapter`] from an RMT channel and an output pin.
-    ///
-    /// # Panics
-    ///
-    /// This function will panic if it fails to configure the RMT channel.
-    pub fn new<C, O>(channel: C, pin: O) -> Self
-    where
-        C: TxChannelCreator<'ch, Dm>,
-        O: PeripheralOutput<'ch>,
-    {
-        let channel = defmt::expect!(
-            channel.configure_tx(pin, Self::channel_config()),
-            "Failed to configure the RMT channel"
+impl<'d> LEDAdapter<'d> {
+    /// Construct the [`LEDAdapter`]. Before calling this function ensure that the timer is
+    /// configured.
+    pub fn new(
+        ledc: &Ledc<'d>,
+        tim: &'d Timer<'d, LowSpeed>,
+        red: impl PeripheralOutput<'d>,
+        green: impl PeripheralOutput<'d>,
+        blue: impl PeripheralOutput<'d>,
+    ) -> Result<Self, channel::Error> {
+        defmt::info!(
+            "LED Timer: freq_hw = {:?}",
+            tim.freq_hw().unwrap_or(Rate::from_hz(0))
         );
+        defmt::info!("LED Timer: freq = {=u32} Hz", tim.frequency());
 
-        Self {
-            channel: Some(channel),
-            buffer: [PulseCode::end_marker(); 25],
-        }
+        let max_duty = if let Some(d) = tim.duty() {
+            d as u32
+        } else {
+            0
+        };
+        defmt::info!("LED Timer: max_duty = {=u32}", (1 << max_duty) - 1);
+
+        let mut ch_red = ledc.channel(channel::Number::Channel0, red);
+        ch_red.configure(channel::config::Config {
+            timer: tim,
+            duty_pct: 0,
+            drive_mode: DriveMode::PushPull,
+        })?;
+
+        let mut ch_green = ledc.channel(channel::Number::Channel1, green);
+        ch_green.configure(channel::config::Config {
+            timer: tim,
+            duty_pct: 0,
+            drive_mode: DriveMode::PushPull,
+        })?;
+
+        let mut ch_blue = ledc.channel(channel::Number::Channel2, blue);
+        ch_blue.configure(channel::config::Config {
+            timer: tim,
+            duty_pct: 0,
+            drive_mode: DriveMode::PushPull,
+        })?;
+
+        Ok(Self {
+            r: ch_red,
+            g: ch_green,
+            b: ch_blue,
+        })
     }
-}
 
-impl<'ch> LEDAdapter<'ch, Blocking> {
-    /// Set the color of the LED. In case an RMT transmission error happens, a warning log message
-    /// is emitted.
-    pub fn set_color(&mut self, color: &RGB) {
-        color.to_pulses(&mut self.buffer);
-        defmt::debug!("Setting LED color to: {:?}", color);
-        defmt::trace!("Transmitting: {=[?; 25]}", self.buffer);
-
-        let ch = defmt::expect!(
-            self.channel.take(),
-            "At this point `self.channel` should be `Some`"
-        );
-
-        match ch.transmit(&self.buffer) {
-            Ok(tx) => match tx.wait() {
-                Ok(ch) => self.channel = Some(ch),
-                Err((e, ch)) => {
-                    defmt::warn!("LED color not set: {}", e);
-                    self.channel = Some(ch);
-                }
-            },
-            Err(_) => {
-                defmt::unreachable!("`self.buffer` is always a valid input to `ch.transmit()`")
-            }
-        }
-    }
-}
-
-impl<'ch> LEDAdapter<'ch, Async> {
-    /// Set the color of the LED. In case an RMT transmission error happens, a warning log message
-    /// is emitted.
-    pub async fn set_color(&mut self, color: &RGB) {
-        color.to_pulses(&mut self.buffer);
-        defmt::debug!("Setting LED color to: {:?}", color);
-        defmt::trace!("Transmitting: {=[?; 25]}", self.buffer);
-
-        let ch = defmt::expect!(
-            self.channel.as_mut(),
-            "We never leave this value as `None` in the async adapter"
-        );
-
-        if let Err(e) = ch.transmit(&self.buffer).await {
-            defmt::warn!("LED color not set: {}", e);
-        }
+    /// Set the LED color using the RGB8 format
+    pub fn set_color(&mut self, r: u8, g: u8, b: u8) {
+        self.r.set_duty_hw(r as u32);
+        self.g.set_duty_hw(g as u32);
+        self.b.set_duty_hw(b as u32);
     }
 }

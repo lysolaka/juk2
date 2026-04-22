0a6b1aad6eac6d0201fcd85e0b2b789f1931c365
lysolaka <lysolaka@gmail.com>
2026-04-22 22:45:25 +0200

    docs(juk-firmware): document the builting strings

--- juk-firmware/src/strings.rs
+++ juk-firmware/src/strings.rs
@@ -2,8 +2,15 @@ use const_format::{concatcp, formatcp};
 
 shadow_rs::shadow!(build);
 
+/// Marker for an info message.
 pub const INFO: &str = "\x1b[1;32m*\x1b[0m ";
+/// Marker for a warning message.
 pub const WARN: &str = "\x1b[1;33m*\x1b[0m ";
+/// Marker for an error message.
+pub const ERROR: &str = "\x1b[1;31m*\x1b[0m ";
+
+const WHITE: &str = "\x1b[1;37m";
+const CLEAR: &str = "\x1b[0m";
 
 const VERSION_VER: &str = formatcp!(
     "{}: {} ({})\r\n",
@@ -13,11 +20,12 @@ const VERSION_VER: &str = formatcp!(
 );
 const VERSION_GIT: &str = formatcp!(
     "Built from {} ({}), on branch `{}`\r\n",
-    build::COMMIT_HASH,
+    build::SHORT_COMMIT,
     build::COMMIT_DATE,
     build::BRANCH
 );
 
+/// License note. States that the program is licensed under GNU GPL-3.
 #[rustfmt::skip]
 pub const LICENSE: &str = concatcp!(
     INFO, "juk-firmware Copyright (C) 2026 lysolaka\r\n",
@@ -26,14 +34,16 @@ pub const LICENSE: &str = concatcp!(
     WARN, "There is ABSOLUTELY NO WARRANTY, to the extent permitted by law.\r\n"
 );
 
+/// Version note: package version and commit information.
 #[rustfmt::skip]
 pub const VERSION: &str = concatcp!(
     INFO, VERSION_VER,
     INFO, VERSION_GIT
 );
 
+/// Welcome message.
 #[rustfmt::skip]
 pub const WELCOME: &str = concatcp!(
     INFO, "Welcome to JUK2\r\n",
-    INFO, "Type `?` anytime for help\r\n"
+    INFO, "Type ", WHITE, "?", CLEAR, " anytime for help\r\n"
 );
